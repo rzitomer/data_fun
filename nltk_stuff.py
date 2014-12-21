62c4a63a6e46feba5db13efeb220b9d2b818362e
Richie <richiezitomer@gmail.com>
2014-12-21 10:44:07 -0800

    nltk book work

---
+++ nltk_stuff.py
@@ -0,0 +1,8 @@
+def generate_model(cfdist,word, num=15):
+	for i in range(num):
+		print word,
+		word =cfdist[word].max()
+
+text = nltk.corpus.genesis.words('english-kjv.txt')
+bigrams = nltk.bigrams(text)
+cfd = nltk.ConditionalFreqDist(bigrams))
